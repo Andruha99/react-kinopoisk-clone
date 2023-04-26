e08c8a834e818ea5988495c27200ee6500588f8d
Andruha99 <Andriei.korovin.2014@mail.ru>
2023-04-26 19:47:14 +0300

    init (D): add project structure

---
+++ src/ui/colors.ts
@@ -0,0 +1,23 @@
+export const primaryColor = {
+  systemPrimary: "#7B61FF",
+};
+
+export const secondaryColor = {
+  systemSecondary: "#917CFF",
+};
+
+export const systemDarkColor = {
+  systemDark: "#80858B",
+};
+
+export const backgroundBlackColor = {
+  backgroundBlack: "#000000",
+};
+
+export const backgroundDarkColor = {
+  backgroundDark: "#242426",
+};
+
+export const backgroundGraphiteColor = {
+  backgroundGraphite: "#323537",
+};
