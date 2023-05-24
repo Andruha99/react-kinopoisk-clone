59d086c696daef1185d831e6bcb08d801eb7c143
Andruha99 <Andriei.korovin.2014@mail.ru>
2023-05-24 19:58:49 +0300

    feat (header): add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { PixemaLight } from "assets";
+import { HeaderContainer } from "./styles";
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <PixemaLight />
+    </HeaderContainer>
+  );
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  position: sticky;
+  top: 0;
+  z-index: 2;
+`;
+
+export { HeaderContainer };
