065e21c8c991f1eb27f645e0e082bee4dfae82cd
Andruha99 <Andriei.korovin.2014@mail.ru>
2023-05-25 15:56:02 +0300

    feat (input): add input component

---
+++ src/components/InputSearch/InputSearch.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyledInputSearch } from "./styles";
+
+export const InputSearch = () => {
+  return <StyledInputSearch></StyledInputSearch>;
+};
---
+++ src/components/InputSearch/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledInputSearch = styled.input`
+  border-radius: 10px;
+`;
+
+export { StyledInputSearch };
